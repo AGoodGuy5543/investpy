2c59898600ddf9eb6572d77ad3dfb3c458ea89b4
BCraine <brandoncraine193@gmail.com>
2021-04-20 18:24:01 -0400

    main updated should work for alex now

--- investpy/main.py
+++ investpy/main.py
@@ -1,19 +1,13 @@
 import investpy
 import pandas as pd
 
-# tester = investpy.get_stock_historical_data(stock='AAPL', country='United States')
 
-# tester = investpy.get_stock_financials(stock='AAPL', country='United States')
+def main():
+    # data = investpy.get_stock_financial_summary(stock='GME', country='United States')
+    # print(data.head())
 
-# Dennis's Problem got rid of if statement
-search_results = investpy.search_quotes(text='apple',
-                                       products=['stocks'],
-                                       countries=['united states'],
-                                       n_results=10)
-for search_result in search_results[:1]:
-    print(search_result)
-    search_result.retrieve_historical_data(from_date='01/01/2019', to_date='01/01/2020')
-    print(search_result.data.head())
+    # temppls = investpy.get_stocks("United States")
+    # print(temppls)
 
     # data = investpy.get_stock_financials(stock='AAPL', country='United States')
     # print("Before")
@@ -23,15 +17,37 @@ for search_result in search_results[:1]:
     # tester = investpy.get_stock_historical_data(stock='AAPL', country='United States')
     # print(tester)
 
+    # SOLO ISSUE TESTS
     # data = investpy.technical_indicators(name='bbva', country='spain', product_type='stock', interval='daily')
-    # data = investpy.technical_indicators(name='aapl', country='united states', product_type='stock', interval='daily')
-    # data = investpy.technical_indicators(name='gme', country='united states', product_type='stock', interval='daily')
-    # data = investpy.technical_indicators(name='U.S. 10Y', country='united states', product_type='bond', interval='daily')
-    # data = investpy.technical_indicators(name='gme', country='united states', product_type='stock', interval='weekly')
-
     # print(data)
-    # data = investpy.get_index_recent_data(index='ibex 35', country='spain')
-    # group issue 2 print(investpy.get_stocks())
+    #
+    # data = investpy.technical_indicators(name='aapl', country='united states', product_type='stock', interval='daily')
+    # print(data)
+    #
+    # data = investpy.technical_indicators(name='gme', country='united states', product_type='stock', interval='daily')
+    # print(data)
+    #
+    # data = investpy.technical_indicators(name='U.S. 10Y', country='united states', product_type='bond',
+    #                                      interval='daily')
+    # print(data)
+    #
+    # data = investpy.technical_indicators(name='DOW 30', country='United States', product_type='index',
+    #                                      interval='15mins')
+    # print(data)
+    #
+    # data = investpy.technical_indicators(name='gme', country='united states', product_type='stock', interval='weekly')
+    # print(data)
+    #
+    # data = investpy.technical_indicators(name='AMZN', country='united states', product_type='stock', interval='1min')
+    # data_other = investpy.technical_indicators(name='AMZN', country='united states', product_type='stock',
+    #                                           interval='monthly')
+    # print(data)
+    # print("(-----------------------------------------------------------------------)")
+    # print("|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|")
+    # print("(-----------------------------------------------------------------------)")
+    # print(data_other)
+
+    # print(investpy.get_stocks())
 
     data = investpy.get_index_historical_data(index='Dow Jones US',
                                               country='united states',
@@ -42,9 +58,7 @@ for search_result in search_results[:1]:
 
     print(data.head())
 
-    # data = investpy.get_indices_list(country='United States')
-    # print(data)
-
 
 if __name__ == '__main__':
     main()
+
